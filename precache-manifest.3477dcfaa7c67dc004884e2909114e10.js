9e688f2c569e196178327321074dba4d6848016b
Diwakar <ddn.dabbara@gmail.com>
2020-02-26 16:54:30 +0530

    Updates

---
+++ precache-manifest.3477dcfaa7c67dc004884e2909114e10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ac2ea13bf2ed43d4cd0ede590d3c52",
+    "url": "/dewalker07/venturesoft-dashboard/index.html"
+  },
+  {
+    "revision": "74ae1f1240369d15ec37",
+    "url": "/dewalker07/venturesoft-dashboard/static/css/main.05632172.chunk.css"
+  },
+  {
+    "revision": "4369faa954367f37ce98",
+    "url": "/dewalker07/venturesoft-dashboard/static/js/2.40a93751.chunk.js"
+  },
+  {
+    "revision": "e2e338faca1a0ff15643f9100df2b490",
+    "url": "/dewalker07/venturesoft-dashboard/static/js/2.40a93751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74ae1f1240369d15ec37",
+    "url": "/dewalker07/venturesoft-dashboard/static/js/main.ed3de0d1.chunk.js"
+  },
+  {
+    "revision": "5cd4297fb49278db1a7a",
+    "url": "/dewalker07/venturesoft-dashboard/static/js/runtime-main.5608827b.js"
+  },
+  {
+    "revision": "879dafbca060dc2a8d805c8aac4b6043",
+    "url": "/dewalker07/venturesoft-dashboard/static/media/logo.879dafbc.png"
+  }
+]);
